b7bb26420c6f9e483cb125ae4822317ed6acc635
wjb <if43vip@gmail.com>
2026-03-12 18:23:14 +0800

    update

--- app.js
+++ app.js
@@ -9,7 +9,52 @@ const MYWXAPI = require('./components/my-wxapi/index.js')
 MYWXAPI.init('v1')
 
 App({
-  onLaunch() {
+  // 全局数据定义
+  globalData: {
+    userInfo: null,     // 微信用户信息
+    openid: null,       // 当前用户的 openid
+  },
+
+  // 小程序启动时自动执行
+  onLaunch: function() {
     console.log('欢迎来到枕边江湖！');
+
+    // 登录，获取 openid（可存在 globalData）
+    wx.login({
+      success: res => {
+        // 调用后端接口换取 openid（建议把 code 发给你的后端）
+        wx.request({
+          url: 'https://dj.awsl8.com/v1/chat/login',
+          method: 'POST',
+          data: {
+            code: res.code   // 微信临时 code
+          },
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          success: result => {
+            if (result.data && result.data.user) {
+              this.globalData.openid = result.data.user.openid;
+              this.globalData.userInfo = result.data.user;
+            }
+          }
+        });
+      }
+    });
+  },
+
+  // 全局获取用户信息的方法（页面可调用）
+  getUserInfo(callback) {
+    if (this.globalData.userInfo) {
+      typeof callback === 'function' && callback(this.globalData.userInfo);
+    } else {
+      wx.getUserInfo({
+        success: res => {
+          this.globalData.userInfo = res.userInfo;
+          if (callback) callback(res.userInfo);
+        }
+      });
+    }
   }
+
 });
--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,30 +1,30 @@
 Page({
-  data: { userInfo: null, stats: { follow: 0, fans: 0, newMsg: 0 } },
+  //data: { userInfo: null, stats: { follow: 0, fans: 0, newMsg: 0 } },
   onLoad() {
-    wx.getUserInfo({
-      success: (res) => {
-        this.setData({ userInfo: res.userInfo });
-        // 登录，获取用户统计
-        wx.request({
-          url: "https://你的后端/api/user/stats",
-          data: { openid: res.userInfo.openId },
-          success: (statRes) => {
-            this.setData({ stats: statRes.data });
-          }
-        });
-      }
-    });
+    // wx.getUserInfo({
+    //   success: (res) => {
+    //     this.setData({ userInfo: res.userInfo });
+    //     // 登录，获取用户统计
+    //     wx.request({
+    //       url: "https://dj.awsl8.com/v1/chat/stats",
+    //       data: { openid: res.userInfo.openId },
+    //       success: (statRes) => {
+    //         this.setData({ stats: statRes.data });
+    //       }
+    //     });
+    //   }
+    // });
   },
   onLogin(e) {
-    let userInfo = e.detail.userInfo;
-    this.setData({ userInfo });
-    wx.request({
-      url: "https://你的后端/api/user/login",
-      method: "POST",
-      data: userInfo,
-      success: (res) => {
-        // 登录成功
-      }
-    });
+    let userInfo = e.detail.userInfo;console.log(e);
+    // this.setData({ userInfo });
+    // wx.request({
+    //   url: "https://dj.awsl8.com/v1/chat/login",
+    //   method: "POST",
+    //   data: userInfo,
+    //   success: (res) => {
+    //     // 登录成功
+    //   }
+    // });
   }
 });
